ccfcb2884add451de5f1bd1a73a0edf9edfe4714
Katerina Ravenwolf <kmmoss@cambridgeport90.org>
2024-07-25 12:16:27 -0400

    separated available scripts into Chocolatey and Winget files
    Originally, there was just one file here, but I decided to make them two; one for Winget installs, and one for Chocolatey installs. the latter also installs the Chocolatey software if not installed.

---
+++ Winget Install.ps1
@@ -0,0 +1,8 @@
+## Winget Install: add to this as many entries as you'd like, but these are starters for the particular format that WinGet asks for, as oppose to Chocolatey. 
+winget install firefox 
+winget install nvda 
+winget install microsoft.teams 
+winget install chrome 
+winget install discord
+winget install python
+            
--- workstation configuration.ps1
+++ workstation configuration.ps1
@@ -1,10 +1,8 @@
 ## Workstation configuration
 ## this script installs several standard applications
 ## firefox, chrome, Office 365 apps, Teams, and NVDA
-## version 1.0 written 8/12/2023 at 19:51 EST
-winget install firefox 
-winget install nvda 
-winget install microsoft.teams 
-winget install chrome 
-winget install microsoft.office
-write-host "Install's done."
+## version 2.0 written 8/12/2023 at 19:51 EST
+## Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+write-host "Chocolatey has been installed. Now installing aplications."
+choco install chrome; firefox; dbatools; au; libreoffice; vscode; logseq; starship; -y 
